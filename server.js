90b7e90c8976ca7e12e6abf0d3e3936f21d09b05
JonathanRQ <jonathan.ruizquintero@gmail.com>
2024-10-31 19:35:49 -0400

    Main directory added and homepage content finished

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get("/", async (req, res) => {
     res.render("index.ejs");
   });
 
+  app.get("/home", async (req, res) => {
+    res.render("index.ejs");
+  });
+
+  app.get("/about", async (req, res) => {
+    res.render("about.ejs");
+  });
+
+  app.get("/contact", async (req, res) => {
+    res.render("contact.ejs");
+  });
+
 app.listen(3000, () => {
     console.log("Listening on port 3000");
   });
