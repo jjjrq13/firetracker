3276b7fcb7b2a5d91401246c1709bbe55802fb87
JonathanRQ <jonathan.ruizquintero@gmail.com>
2024-10-31 18:36:37 -0400

    Server up and running

---
+++ server.js
@@ -0,0 +1,15 @@
+
+// ------------- PACKAGES -------------
+const express = require("express");
+const app = express();
+
+
+
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+  });
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+  
