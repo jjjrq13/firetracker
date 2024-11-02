64d378ad3636f1cd9709bbe2062e9c6b3cc36d44
JonathanRQ <jonathan.ruizquintero@gmail.com>
2024-11-01 22:06:45 -0400

    posting to the mango!

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -7,14 +7,24 @@ router.get('/', async (req, res) => {
     try {
         const currentUser = await Transactions.findById(req.session.user._id);
 
-        const userExpenses = 0;
-        const userIncome = 0;
+        let userExpenses = 0;
+        let userIncome = 0;
+
+
+        console.log(currentUser.transactions);
+
+        currentUser.transactions.sort((a,b) => b.date - a.date);
 
         currentUser.transactions.forEach((transaction) => {
+            console.log(`This is what is printing: ${transaction}`);
+
+            console.log('This is Tpye:', transaction.type);
+            console.log('this is Amount:', transaction.amount);
             if (transaction.type === 'Expense') {
-                userExpenses += transaction.amount;
-            } else {
-                userIncome += transaction.amount;
+                userExpenses = transaction.amount + userExpenses;
+            } 
+            else if (transaction.type === 'Income'){
+                userIncome = transaction.amount + userIncome;
             }
         });
 
@@ -26,6 +36,27 @@ router.get('/', async (req, res) => {
             income: userIncome,
             transactions: currentUser.transactions,
         });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+router.get('/new', async (req, res) => {
+    res.render('transactions/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await Transactions.findById(req.session.user._id);
+
+        currentUser.transactions.push(req.body);
+
+        await currentUser.save();
+
+        console.log(req.body);
+
+        res.redirect(`/users/${currentUser.username}/transactions`);
     } catch (error) {
         console.log(error);
         res.redirect('/');
--- models/transactions.js
+++ models/transactions.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const transactionsSchema = new mongoose.Schema({
+    date: { type: Date, default: Date.now, required: true },
     description: { type: String, maxlength: 50 },
     amount: { type: Number, required: true },
-    date: { type: Date, default: Date.now, required: true },
-    type: { 
-        type: String, 
-        enum: ['Income', 'Expense'], 
-        required: true 
+    type: {
+        type: String,
+        enum: ['Income', 'Expense'],
+        required: true,
     },
     category: {
         type: String,
@@ -15,15 +15,14 @@ const transactionsSchema = new mongoose.Schema({
         required: true,
     },
     notes: { type: String, maxlength: 200 },
-    number: { type: Number },
 });
 
 const transactionsUserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     firstName: { type: String, required: true },
-    lastName: {type: String, required: true},
+    lastName: { type: String, required: true },
     password: { type: String, required: true },
-    balance: { type: Number, default: 0 }, 
+    balance: { type: Number, default: 0 },
     transactions: [transactionsSchema],
 });
 
